b2d21af8f808615f090e58115eca8e0a6e543d92
Niushaaaa <117599554+Niushaaaa@users.noreply.github.com>
2023-05-16 00:22:46 -0700

    ezafe krdn code hay delete ba switch va link krdn jdval class va student bray delete

--- delete.php
+++ delete.php
@@ -1,1 +1,22 @@
 <?php
+session_start();
+ob_start();
+include "connection.php";
+if (isset($_GET['id']) && isset($_GET['page'])) {
+    $id = $_GET['id'];
+    $page = $_GET['page'];
+    switch ($page) {
+        case 1;
+            $query = "DELETE FROM class WHERE shomare_danshamoz=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+        case 2:
+            $query = "DELETE FROM student WHERE shomare_class=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+    }
+}
